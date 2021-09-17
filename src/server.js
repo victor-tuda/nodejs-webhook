e6478b4cbca9b39d5125ed4c925f6da72c6c01fd
victor-tuda <victor_gtuda@outlook.com>
2021-09-17 11:14:13 -0300

    Primeiro commit de produção

--- src/server.js
+++ src/server.js
@@ -1,13 +1,27 @@
+// Criando um Debugger Morgan
 const morgan = require('morgan');
 
+// Adicionando as variáveis de ambiente para desenvolvimento local
 require('dotenv').config({
     path: __dirname+'./../.env'
 });
 
+// Adicionando as comunicações com o banco de dados
 var database = require("./database/index.js")
 
+// Realizando um select no database
 database.select('*').from('Webhook_FDV').then(data => {
     console.log(data);
 }).catch(err => {
     console.log(err);
 });
+
+// NETILIFY
+exports.handler = async (event, context) => {
+    const body = JSON.parse(event.body);
+    const logbook = body.Logbook[body.Logbook.length - 1];
+
+    database('Webhook_FDV').insert({CODE: body.Code})
+     .then(console.log('Executou'))
+
+  };

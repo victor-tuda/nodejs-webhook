eff716e7d17a0012429e66a9fdb4ed768048b781
victor-tuda <victor_gtuda@outlook.com>
2021-09-20 13:39:23 -0300

    Tentando realizar um rollback

--- src/database/index.js
+++ src/database/index.js
@@ -1,5 +1,4 @@
 const knexfile = require('../../knexfile');
-console.log(knexfile)
 const knex = require('knex')(knexfile.production);
 
 module.exports = knex;
--- src/server.js
+++ src/server.js
@@ -1,49 +1,26 @@
-require('tedious');
-require('mssql');
-
 // Criando um Debugger Morgan
 const morgan = require('morgan');
-
+require('tedious')
 
 // Adicionando as variáveis de ambiente para desenvolvimento local
-/*
 require('dotenv').config({
     path: __dirname+'./../.env'
 });
-*/
-// Adicionando as comunicações com o banco de dados
-var database = require("/database/index.js");
 
-console.log(database);
+// Adicionando as comunicações com o banco de dados
+var database = require("./database/index.js")
+
+// Realizando um select no database
+database.select('*').from('Webhook_FDV').then(data => {
+    console.log(data);
+});
 
 // NETILIFY
 exports.handler = async (event, context) => {
-    try{
-        const body = JSON.parse(event.body);
-        const logbook = body.Logbook[body.Logbook.length - 1];
+    const body = JSON.parse(event.body);
+    const logbook = body.Logbook[body.Logbook.length - 1];
 
-        /*
-        var dados = {
-            CODE: body.Code,
-            TITLE: body.Title
-        }
+    database('Webhook_FDV').insert({CODE: body.Code})
+     .then(console.log('Executou'))
 
-        database.insert(dados).into('Webhook_FDV').then(data => {
-            console.log(data);
-        }).catch(err => {
-            console.log(err)
-        });
-        */
-
-        database.select("*").from("Webhook_FDV").then(data => {
-            console.log(data);
-            console.log('Cheguei aqui')
-        }).catch(err => {
-            console.log(err)
-        })
-    }
-        
-    catch (error){
-        console.log(`Erro de Conexão: ${error}`);
-    };
-}
+  };

45458261da787392dcf3f4ed797f43c468181878
TI-PROFITTARE02 <thiago@profittare.com.br>
2021-09-23 14:02:37 -0300

    Corrigido BUG em pool new

--- src/server.js
+++ src/server.js
@@ -10,32 +10,33 @@ require('dotenv').config({
     path: __dirname + './../.env'
 });
 
+
+// Criando uma pool de conexão
+const pool = new sql.ConnectionPool(connStr);
+const poolConnect = pool.connect();
+
+// Criando uma função assíncrona para conectar ao banco de dados
+async function messageHandler(){
+    await poolConnect; // ensures that the pool has been created
+    try {
+        const request = pool.request(); // or: new sql.Request(pool1)
+        const result = await request.query('SELECT * FROM Webhook_FDV')
+        console.log(result)
+        return result;
+    } catch (err) {
+        console.error('SQL error', err);
+    }
+}
+
 // Adicionando as comunicações com o banco de dados KNEXJS
 // var database = require("./database/index.js");
 
 // NETILIFY
 exports.handler = async (event, context) => {
-// Criando uma pool de conexão
-const pool = new sql.ConnectionPool(connStr);
-const poolConnect = pool.connect();
-
     const body = JSON.parse(event.body)
     const logbook = body.Logbook[body.Logbook.length - 1];
 
-    // Criando uma função assíncrona para conectar ao banco de dados
-    async function messageHandler(){
-        await poolConnect; // ensures that the pool has been created
-        try {
-            const request = pool.request(); // or: new sql.Request(pool1)
-            const result = await request.query('SELECT * FROM Webhook_FDV')
-            console.log(result)
-            return result;
-        } catch (err) {
-            console.error('SQL error', err);
-        }
-    }
-
-
+    messageHandler();
 
     console.dir(`Linha Inserida: ${pool.rowsAffected}`) 
 

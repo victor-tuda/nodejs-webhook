54f24e9f5646f61c78c9f8268786b5b617b26f35
TI-PROFITTARE02 <thiago@profittare.com.br>
2021-09-28 10:05:26 -0300

    tentando visualizar o evento

--- src/server.js
+++ src/server.js
@@ -1,14 +1,6 @@
-// Criando um Debugger Morgan
-// const morgan = require('morgan');
-
 // Criando uma String de Conexão com o Banco de Dados
 var connStr = `Server=${process.env.HOST},${process.env.PORT};Database= ${process.env.DATABASE};User Id= ${process.env.USER};Password=${process.env.PASSWORD};trustServerCertificate=true;`;
 var sql = require("mssql");
-
-// Adicionando as variáveis de ambiente para desenvolvimento local
-require('dotenv').config({
-    path: __dirname + './../.env'
-});
 
 // Crindo uma função de conexão
 const conexao_db = async function() {
@@ -18,25 +10,18 @@ const conexao_db = async function() {
   return result
 }
 
-
-// Adicionando as comunicações com o banco de dados KNEXJS
-// var database = require("./database/index.js");
-
 // NETILIFY
 exports.handler = async function(event, context) {
   request = conexao_db();
-  console.log(request);
+  console.log(`REQUEST: ${request}`);
+  console.log(`CONTEXT: ${context}`);
 
   const body = JSON.parse(event.body); //Criando uma variável para capturar o body da requisição
-  console.log(body);
-  console.log(body.Code);
 
   request.input(code, NVarChar(4000), `${body.Code}`);
-  request.query(`INSERT INTO Webhook_FDV (CODE) VALUES (@code)`);
-
-
+  request.query(`INSERT INTO Webhook_FDV (CODE) VALUES (${body.Code})`);
   
-      console.dir(`Linha Inserida: ${result.rowsAffected}`)
+  console.dir(`Linha Inserida: ${result.rowsAffected}`)
   
 
   

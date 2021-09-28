612a079458b475da206625823f0c37df8784ac33
TI-PROFITTARE02 <thiago@profittare.com.br>
2021-09-28 09:22:15 -0300

    tentando visualizar o retorno

--- src/server.js
+++ src/server.js
@@ -1,6 +1,3 @@
-
-'use strict'
-
 // Criando um Debugger Morgan
 // const morgan = require('morgan');
 
@@ -14,19 +11,6 @@ require('dotenv').config({
 });
 
 
-
-
-// Criando uma função assíncrona para conectar ao banco de dados
-function messageHandler(request){
-    try {
-        request.query('SELECT * FROM Webhook_FDV');
-        console.log(result);
-        return result;
-    } catch (err) {
-        console.error('SQL error', err);
-    }
-}
-
 // Adicionando as comunicações com o banco de dados KNEXJS
 // var database = require("./database/index.js");
 
@@ -34,20 +18,18 @@ function messageHandler(request){
 exports.handler = async (event, context) => {
     try {
       const body = JSON.parse(event.body); //Criando uma variável para capturar o body da requisição
-      const jsonString = JSON.stringify(body);
-      const logbook = JSON.stringify(body.Logbook[body.Logbook.length - 1]);
-      console.log(jsonString);
   
-      let pool = await sql.connect(connStr); 
-      let result = await pool.request()
-        .input(code, NVarChar(4000), `${body.Code}`)
-        .query(`INSERT INTO Webhook_FDV (CODE) VALUES (@code)`)
+      let pool = await sql.connect(connStr);
+      let result = await pool.request();
+
+      result.input(code, NVarChar(4000), `${body.Code}`)
+      result.query(`INSERT INTO Webhook_FDV (CODE) VALUES (@code)`)
   
       console.dir(`Linha Inserida: ${result.rowsAffected}`)
     }
   
-    catch (err) {
-      console.log(`Code: ${body.Code}\nErro na Inserção: ${err}`)
+    catch(err) {
+      console.log(`Code: ${body.Code}\nErro na Response: ${err}`)
       return { statusCode: 500, body: err.toString() }
     }
   

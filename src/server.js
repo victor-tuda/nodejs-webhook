29df06faae5f3ce4612be2b7541709867b5fe011
TI-PROFITTARE02 <thiago@profittare.com.br>
2021-09-28 10:40:20 -0300

    Rollback

--- src/server.js
+++ src/server.js
@@ -2,30 +2,19 @@
 var connStr = `Server=${process.env.HOST},${process.env.PORT};Database= ${process.env.DATABASE};User Id= ${process.env.USER};Password=${process.env.PASSWORD};trustServerCertificate=true;`;
 var sql = require("mssql");
 
-// Crindo uma função de conexão
-conexao_db = sql.connect(connStr, function (err) {
-  if (err){
-    console.log(`Erro na conexão com o Banco de Dados: ${err}`)
-  }
-  const result = new sql.Request();
-
-  return result;
-  }
-)
-
 
 // NETILIFY
-exports.handler = async function(event, context) {
-  request = conexao_db();
+exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  const logbook = body.Logbook[body.Logbook.length - 1];
 
-  const body = JSON.parse(event.body); //Criando uma variável para capturar o body da requisição
+  let pool = await sql.connect(connStr);
+  let result = await pool.request()
+  .input('code', 'sql.NChar(150)', body.Code, 'CODE')
+  .input('title', 'sql.NChar(150)', body.Title, 'TITLE')
+  .input('value', 'sql.Float', body.Value, 'VALUE')
+  .query(`INSERT INTO Webhook_FDV (CODE, TITLE, VALUE) VALUES (@code, @title, @value)`)
 
-  request.input('code', sql.NVarChar(150), `${body.Code}`);
-  request.query(`INSERT INTO Webhook_FDV (CODE) VALUES (${body.Code})`);
-  
-  console.dir(`Linha Inserida: ${result.rowsAffected}`)
-  
+  console.dir(`Linha Inserida: ${result.rowsAffected}`) 
 
-  
-  };
-  
+};

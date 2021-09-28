a98b093d372b7205b5848bbb19e218f12593243e
TI-PROFITTARE02 <thiago@profittare.com.br>
2021-09-28 14:01:13 -0300

    Inserindo os inputs de Company

--- src/server.js
+++ src/server.js
@@ -29,12 +29,26 @@ exports.handler = async (event, context) => {
   .input('contact_role', sql.NVarChar(150), body.Contact.Role)
   .input('contact_document', sql.NVarChar(150), body.Contact.Document)
 
+  .input('company_code', sql.NVarChar(150), body.Contact.Company.Code)
+  .input('company_name', sql.NVarChar(150), body.Contact.Company.Name)
+  .input('company_segment', sql.NVarChar(150), body.Contact.Company.Segment)
+  .input('company_document', sql.NVarChar(150), body.Contact.Company.Document)
+  .input('company_state_document', sql.NVarChar(150), body.Contact.Company.State_Document)
+  .input('company_address', sql.NVarChar(150), body.Contact.Company.Address)
+  .input('company_latitude', sql.NVarChar(150), body.Contact.Company.Latitude)
+  .input('company_longitude', sql.NVarChar(150), body.Contact.Company.Longitude)
+
+
+
   .query(`INSERT INTO Webhook_FDV (CODE, TITLE, VALUE, STATUS, ANSWER,\
     LOGBOOK_CODE, LOGBOOK_TYPE, LOGBOOK_TEXT, LOGBOOK_CREATED_AT,\
-    CONTACT_CODE, CONTACT_NAME, CONTACT_EMAIL, CONTACT_PHONE, CONTACT_SMARTPHONE, CONTACT_ROLE, CONTACT_DOCUMENT)
+    CONTACT_CODE, CONTACT_NAME, CONTACT_EMAIL, CONTACT_PHONE, CONTACT_SMARTPHONE, CONTACT_ROLE, CONTACT_DOCUMENT,\
+    COMPANY_CODE, COMPANY_NAME, COMPANY_SEGMENT, COMPANY_DOCUMENT, COMPANY_STATE_DOCUMENT, COMPANY_ADDRESS, COMPANY_LATITUDE, COMPANY_LONGITUDE\
+    )
     VALUES (@code, @title, @value, @status, @answer,\
       @logbook_code, @logbook_type, @logbook_text, @logbook_created_at,\
-      @contact_code, @contact_name, @contact_email, @contact_phone, @contact_smartphone, @contact_role, @contact_document\
+      @contact_code, @contact_name, @contact_email, @contact_phone, @contact_smartphone, @contact_role, @contact_document,\
+      @company_code, @company_name, @company_segment, @company_document, @company_state_document, @company_address, @company_latitude, @company_longitude\
       )`)
 
   console.dir(`Linha Inserida: ${result.rowsAffected}`) 

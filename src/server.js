1ef72e3a545e8b49dd9151f8c92a4c00cd3fedc4
TI-PROFITTARE02 <thiago@profittare.com.br>
2021-09-28 13:48:11 -0300

    Inserindo os inputs de Contato

--- src/server.js
+++ src/server.js
@@ -18,11 +18,24 @@ exports.handler = async (event, context) => {
 
   .input('logbook_code', sql.NVarChar(150), logbook.Code)
   .input('logbook_type', sql.NVarChar(150), logbook.Type)
+  .input('logbook_text', sql.NVarChar(150), logbook.Text)
+  .input('logbook_created_at', sql.NVarChar(150), logbook.Created_At)
+
+  .input('contact_code', sql.NVarChar(150), body.Contact.Code)
+  .input('contact_name', sql.NVarChar(150), body.Contact.Name)
+  .input('contact_email', sql.NVarChar(150), body.Contact.Email)
+  .input('contact_phone', sql.NVarChar(150), body.Contact.Phone)
+  .input('contact_smartphone', sql.NVarChar(150), body.Contact.SmartPhone)
+  .input('contact_role', sql.NVarChar(150), body.Contact.Role)
+  .input('contact_document', sql.NVarChar(150), body.Contact.Document)
 
   .query(`INSERT INTO Webhook_FDV (CODE, TITLE, VALUE, STATUS, ANSWER,\
-    LOGBOOK_CODE, LOGBOOK_TYPE)
-    VALUES (@code, @title, @value, @status, @answer,\
-      @logbook_code, @logbook_type)`)
+    LOGBOOK_CODE, LOGBOOK_TYPE, LOGBOOK_TEXT, LOGBOOK_CREATED_AT,\
+    CONTACT_CODE, CONTACT_NAME, CONTACT_EMAIL, CONTACT_PHONE, CONTACT_SMARTPHONE, CONTACT_ROLE, CONTACT_DOCUMENT)
+    VALUES (@code, @title, @value, @status, @answer
+      @logbook_code, @logbook_type, @logbook_text, @logbook_created_at,\
+      @contact_code, @contact_name, @contact_email, @contact_phone, @contact_smartphone, @contact_role, @contact_document
+      )`)
 
   console.dir(`Linha Inserida: ${result.rowsAffected}`) 
 
